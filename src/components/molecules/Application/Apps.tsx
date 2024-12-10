f3472350e781829156d114e4dde3818bbd2e9b45
AhmetEris-dev <ahmet2_lock@hotmail.com>
2024-12-10 22:28:35 +0300

    düzenlemeler yapıldı

---
+++ src/components/molecules/Application/Apps.tsx
@@ -0,0 +1,117 @@
+import React from "react";
+
+function Apps() {
+  return (
+    <>
+      <div className="container">
+      <div className="row">
+        <div className="col-3">
+          <p className="text-center" style={{fontWeight:'bolder',fontSize:'35px',}}>Uygulamalar</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius accusamus illum velit </p>
+        </div>
+        <div className="col-3">
+          <div className="card" style={{ width: "18rem;" }}>
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="card" style={{ width: "18rem;" }}>
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="card" style={{ width: "18rem;" }}>
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-3">
+        <div className="card" style={{ width: "18rem;" }}>
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-3">
+        <div className="card" style={{ width: "18rem;" }}>
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-3">
+        <div className="card" style={{ width: "18rem;" }}>
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-3">
+        <div className="card" style={{ width: "18rem;" }}>
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    </>
+  );
+}
+export default Apps;
--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -1,52 +1,86 @@
-import React from 'react'
-import './Header.css'
- 
+import React from 'react';
+import './Header.css';
 
 function Header() {
   return (
-    
-    <nav className="navbar navbar-expand-lg bg-color">
-  <div className="container-fluid">
-    <div className="col-4 text-center">
-    <a className="navbar-brand ms-5" style={{fontWeight:'bold'}} href="#">JAVA İk</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
+    <div className="container-fluid p-0"> 
+      <nav className="navbar navbar-expand-lg bg-color">
+        <div className="row w-100 m-0"> 
+          <div className="col-4 text-center">
+            <a className="navbar-brand ms-5" style={{ fontWeight: 'bold' }} href="#">
+              JAVA İk
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavDropdown"
+              aria-controls="navbarNavDropdown"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+          </div>
+          <div className="col-8 d-flex justify-content-end">
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Features
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Pricing
+                  </a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Dropdown link
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Another action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+              <button type="button" className="btn btn-primary mx-2">
+                Primary
+              </button>
+              <button type="button" className="btn btn-primary">
+                Primary
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
-    <div className="col-8  ">
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav ">
-        <li className="nav-item ">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item ">
-          <a className="nav-link" href="#">Features</a>
-        </li>
-        <li className="nav-item ">
-          <a className="nav-link" href="#">Pricing</a>
-        </li>
-        <li className="nav-item dropdown ">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
-      <button type="button" className="btn btn-primary mx-2 ">Primary</button>
-      <button type="button" className="btn btn-primary " >Primary </button>
-    </div>
-  </div>
-    </div>
-    
-</nav>
-    
-    
-  
-  )
+  );
 }
 
-export default Header
+export default Header;
