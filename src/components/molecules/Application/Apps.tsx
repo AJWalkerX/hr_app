ceccab389edb6c47e0317e7dbca02707a53424d0
EmineKarabolat <emine.karabolat.01@gmail.com>
2024-12-11 16:54:28 +0300

    apps menüye geçildi

--- src/components/molecules/Application/Apps.tsx
+++ src/components/molecules/Application/Apps.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import './Apps.css'
 function Apps() {
   return (
     <>
@@ -10,16 +10,31 @@ function Apps() {
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius accusamus illum velit </p>
         </div>
         <div className="col-3">
-          <div className="card" style={{ width: "18rem;" }}>
-            <div className="card-body">
-              <h5 className="card-title">Special title treatment</h5>
-              <p className="card-text">
-                With supporting text below as a natural lead-in to additional
-                content.
+        <div className="card" style={{ width: "18rem;" }}>
+        <div className="card-body">
+          <div className="row">
+            <div className="col-1 mt-1">
+            <i className="fa-solid fa-user ms-0"></i>
+            </div>
+            <div className="col-11 ">            
+            <span className="apps-text-style">Personel Yönetimi</span>
+            </div>
+            </div>
+          <div className="row">
+          <p className="card-text">
+               Çalışanların tüm bilgilerini tek uygulamada yönetin
               </p>
-              <a href="#" className="btn btn-primary">
+          </div>
+          <div className="row">
+          <a href="#" className="btn btn-primary">
                 Go somewhere
               </a>
+          </div>
+
+
+          
+              
+              
             </div>
           </div>
         </div>
@@ -52,7 +67,7 @@ function Apps() {
           </div>
         </div>
       </div>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-3">
         <div className="card" style={{ width: "18rem;" }}>
             <div className="card-body">
