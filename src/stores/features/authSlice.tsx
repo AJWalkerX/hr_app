b037ef0b3a0bd61244872978cf2fbf4deb99712a
Alexander Joseph Walker <a.j.walker99@outlook.com>
2024-12-12 12:56:47 +0000

    store added

--- src/stores/features/authSlice.tsx
+++ src/stores/features/authSlice.tsx
@@ -1,9 +1,12 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { create } from "domain";
 import { ILoginRequest } from "../../models/ILoginRequest";
 import apis from "../../config/RestApis";
+import { RegisterRequest } from "../../models/IRegisterRequest";
+import Swal from "sweetalert2";
+import { IBaseResponse } from "../../models/IBaseResponse";
 
-const initalAuthState = {
+const initialAuthState = {
   isAuth: false,
   isLoginLoading: false,
   isRegisterLoading: false,
@@ -23,3 +26,64 @@ export const fetchLogin = createAsyncThunk(
     return response;
   }
 );
+
+export const fetchRegister = createAsyncThunk(
+  "user/fetchRegister",
+  async (payload: RegisterRequest) => {
+    const response = await fetch(apis.userService + "/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    }).then((data) => data.json());
+    return response;
+  }
+);
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: initialAuthState,
+  reducers: {},
+  extraReducers: (build) => {
+    build.addCase(fetchRegister.pending, (state) => {
+      state.isRegisterLoading = true;
+    });
+    build.addCase(
+      fetchRegister.fulfilled,
+      (state, action: PayloadAction<IBaseResponse>) => {
+        state.isRegisterLoading = false;
+        if (action.payload.code === 200) {
+          Swal.fire({
+            icon: "success",
+            title: action.payload.message,
+            timer: 1500,
+          });
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Hata!",
+            text: action.payload.message,
+          });
+        }
+      }
+    );
+
+    build.addCase(fetchLogin.pending, (state) => {
+      state.isLoginLoading = true;
+    });
+    build.addCase(
+      fetchLogin.fulfilled,
+      (state, action: PayloadAction<IBaseResponse>) => {
+        state.isLoginLoading = false;
+        if (action.payload.code === 200) {
+          state.isAuth = true;
+          localStorage.setItem("token", action.payload.data);
+        } else {
+        }
+      }
+    );
+  },
+});
+
+export default authSlice.reducer;
