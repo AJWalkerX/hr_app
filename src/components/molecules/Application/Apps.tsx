7a72ae6654e8cdc7370766f03e52469423ff59b7
EmineKarabolat <emine.karabolat.01@gmail.com>
2024-12-12 18:01:44 +0300

    app card düzenlendi

--- src/components/molecules/Application/Apps.tsx
+++ src/components/molecules/Application/Apps.tsx
@@ -1,137 +1,96 @@
 import React from "react";
 import './Apps.css'
+import AppsCard from "../../atoms/Apps/AppsCard";
+import { title } from "process";
+
 function Apps() {
+  const appcards = [
+    {
+      imageUrl: "https://cdn.prod.website-files.com/6113889e45c6e62ebf4ca212/6113b539458e093011715772_group-10%20(3).svg",
+      title: 'Personel Yönetimi',
+      description: 'Çalışanların tüm bilgilerini tek uygulamada yönetin', 
+      linkUrl: '#', 
+      hreftitle: 'ŞİMDİ İNCELE', 
+      arrow: 'fa-solid fa-arrow-right'
+
+    },
+    
+
+    {
+      imageUrl: "https://cdn.prod.website-files.com/6113889e45c6e62ebf4ca212/6717b86616ee16efc14e6252_bordro%20icon.svg",
+      title: 'Bordro Yönetimi',
+      description: 'Tüm bordro işlemlerinizi tek ekranda kolayca yönetin', 
+      linkUrl: '#', 
+      hreftitle: 'ŞİMDİ İNCELE', 
+      arrow: 'fa-solid fa-arrow-right'
+
+    },
+    
+    {
+      imageUrl: "https://cdn.prod.website-files.com/6113889e45c6e62ebf4ca212/6113b6c1afed4f887b9abb86_group-8%20(3).svg",
+      title: 'Performans Yönetimi',
+      description: 'Çalışanları online performans değerlendirmeye dahil edin', 
+      linkUrl: '#', 
+      hreftitle: 'ŞİMDİ İNCELE', 
+      arrow: 'fa-solid fa-arrow-right'
+    },
+    {
+      imageUrl: "https://cdn.prod.website-files.com/6113889e45c6e62ebf4ca212/66c4ff4bc3c8acd906e30f7b_talentii.svg",
+      title: 'İşe Alım ve Aday Takip',
+      description: 'İşe alım süreçlerinizi tek bir platformda yönetin', 
+      linkUrl: '#', 
+      hreftitle: 'ŞİMDİ İNCELE', 
+      arrow: 'fa-solid fa-arrow-right'
+    },
+    {
+      imageUrl: "https://cdn.prod.website-files.com/6113889e45c6e62ebf4ca212/6113b6b76e2094438bf1f37c_group-9%20(3).svg",
+      title: 'Vardiya Yönetimi',
+      description: 'Çalışma zamanınızı en verimli şekilde planlayın ve yönetin', 
+      linkUrl: '#', 
+      hreftitle: 'ŞİMDİ İNCELE', 
+      arrow: 'fa-solid fa-arrow-right'
+    },
+    {
+      imageUrl:"https://cdn.prod.website-files.com/6113889e45c6e62ebf4ca212/66c5f3f54ff100f5618fc390_analitikv1.svg",
+      title: 'Ücret Yönetimi',
+      description: 'Maaşları güncelleyin, yönetin ve raporlayın', 
+      linkUrl: '#', 
+      hreftitle: 'ŞİMDİ İNCELE', 
+      arrow: 'fa-solid fa-arrow-right'
+    }
+  ] ;
   return (
-    <>
-      <div className="container">
+   
+  <>
+    <div className="container">
       <div className="row">
-        <div className="col-3">
+        <div className=" text-center mb-4">
           <p className="text-center" style={{fontWeight:'bolder',fontSize:'35px',}}>Uygulamalar</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius accusamus illum velit </p>
+          <p className="text-center" style={{fontWeight:'bolder',fontSize:'25px',}}>Çalışan sayınıza göre fiyatlama, dilediğiniz kadar kullanın, kullandığınız kadar ödeyin!</p>
         </div>
-
-
-        <div className="col-3">
-        <div className="card" style={{ width: "18rem;" }}>
-        <div className="card-body">
-          <div className="row">
-            <div className="col-1 mt-1">
-            <i className="fa-solid fa-user ms-0"></i>
-            </div>
-            <div className="col-11 ">            
-            <span className="apps-text-style">Personel Yönetimi</span>
-            </div>
-            </div>
-          <div className="row">
-          <p className="card-text">
-               Çalışanların tüm bilgilerini tek uygulamada yönetin
-              </p>
-          </div>
-          <div className="row mt-3">
-          
-          <a className="me-5" style={{textDecoration:'none', color:'blue'}} href="#">
-            ŞİMDİ İNCELE
-            <i className="apps-arrow-style fa-solid fa-arrow-right"></i>
-
-          </a>
-          
-          
-            
-            </div>
+      </div>
+      <div className="row mt-4   ">
+      
+        {
+            appcards.map((appcard, index) => (
+              <div className="col-lg-4 col-md-3 col-sm-6 mb-4" key={index}>
+                <AppsCard 
+                  imageUrl={appcard.imageUrl} 
+                  title={appcard.title} 
+                  description={appcard.description} 
+                  linkUrl={appcard.linkUrl} 
+                  hreftitle={appcard.hreftitle} 
+                  arrow={appcard.arrow} 
+                />
+              </div>
+            ))
+          }
        
          
-            </div>
-          </div>
-        </div>
-        <div className="col-3">
-          <div className="card" style={{ width: "18rem;" }}>
-            <div className="card-body">
-              <h5 className="card-title">Special title treatment</h5>
-              <p className="card-text">
-                With supporting text below as a natural lead-in to additional
-                content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col-3">
-          <div className="card" style={{ width: "18rem;" }}>
-            <div className="card-body">
-              <h5 className="card-title">Special title treatment</h5>
-              <p className="card-text">
-                With supporting text below as a natural lead-in to additional
-                content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
-          </div>
         </div>
       </div>
-      <div className="row mt-3">
-        <div className="col-3">
-        <div className="card" style={{ width: "18rem;" }}>
-            <div className="card-body">
-              <h5 className="card-title">Special title treatment</h5>
-              <p className="card-text">
-                With supporting text below as a natural lead-in to additional
-                content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col-3">
-        <div className="card" style={{ width: "18rem;" }}>
-            <div className="card-body">
-              <h5 className="card-title">Special title treatment</h5>
-              <p className="card-text">
-                With supporting text below as a natural lead-in to additional
-                content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col-3">
-        <div className="card" style={{ width: "18rem;" }}>
-            <div className="card-body">
-              <h5 className="card-title">Special title treatment</h5>
-              <p className="card-text">
-                With supporting text below as a natural lead-in to additional
-                content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col-3">
-        <div className="card" style={{ width: "18rem;" }}>
-            <div className="card-body">
-              <h5 className="card-title">Special title treatment</h5>
-              <p className="card-text">
-                With supporting text below as a natural lead-in to additional
-                content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      </div>
-    </>
+  </>
+ 
   );
 }
 export default Apps;
