3f124922ee82627f58ccca21c712b7b2b0721bbb
alperrguler <alperguler142@gmail.com>
2024-12-13 14:26:44 +0300

    Admin Login düzeltildi.

---
+++ src/pages/AdminLoginPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AdminLogin from '../components/molecules/AdminLogin/AdminLogin'
+
+function AdminLoginPage() {
+  return (
+    <>
+       
+        <AdminLogin />
+     
+    </>
+  )
+}
+
+export default AdminLoginPage
