175b916b6dc18b848403c9912556f493a074949b
EmineKarabolat <emine.karabolat.01@gmail.com>
2024-12-13 16:56:52 +0300

    headerda gerekli düzenlemeler yapıldı button ayarları değiştirildi.

--- src/components/molecules/NavButton/NavButtons.tsx
+++ src/components/molecules/NavButton/NavButtons.tsx
@@ -1,19 +1,29 @@
 import React from "react";
 import Button from "../../atoms/NavButton/NavButton";
-import './NavButtons'
+import './NavButtons.css';
 
+// ButtonStyle için bir arayüz tanımlayalım
+interface ButtonStyleProps {
+    color?: string;
+    borderColor?: string;
+    backgroundColor?: string;
+}
 
-
-function NavButtons() {
-  return (
-    <div className="d-flex justify-content-end">
-     
-      <Button cls="nav-button-register" text="KAYIT OL"  />
-      
-      
-      <Button cls="nav-button-login" text="GİRİŞ YAP"  />
-    </div>
-  );
+function NavButtons({ buttonStyle }: { buttonStyle?: ButtonStyleProps }) {
+    return (
+        <div className="d-flex justify-content-end">
+            <Button 
+                cls="nav-button-register" 
+                text="KAYIT OL"
+                style={buttonStyle}
+            />
+            <Button 
+                cls="nav-button-login" 
+                text="GİRİŞ YAP"
+                style={buttonStyle}
+            />
+        </div>
+    );
 }
 
 export default NavButtons;
