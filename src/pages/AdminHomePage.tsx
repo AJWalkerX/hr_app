b615ee1e4167c9fe57ec1e7ce2c318d63d83159b
alperrguler <alperguler142@gmail.com>
2024-12-15 18:18:37 +0300

    admin customers tablosu yapıldı.

--- src/pages/AdminHomePage.tsx
+++ src/pages/AdminHomePage.tsx
@@ -4,9 +4,11 @@ import AdminHome from '../components/molecules/AdminPanel/AdminHome'
 
 function AdminHomePage() {
   return (
+        <div className='container-fluid'>
         <div className='row'>
-            <div className="col-3"> <AdminSidebar/> </div>
-            <div className="col-9"> <AdminHome/></div> 
+            <div className="col-3"><AdminSidebar/> </div>
+            <div className="col-9"><AdminHome/></div> 
+        </div>
         </div>
   )
 }
