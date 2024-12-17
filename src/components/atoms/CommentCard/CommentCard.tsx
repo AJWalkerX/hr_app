e4542b3b9f53c19bb1a776f573315cc01132d3e8
EmineKarabolat <emine.karabolat.01@gmail.com>
2024-12-17 11:48:27 +0300

    usercomment düzenlendi

--- src/components/atoms/CommentCard/CommentCard.tsx
+++ src/components/atoms/CommentCard/CommentCard.tsx
@@ -5,8 +5,8 @@ function CommentCard(props: ICommentCard) {
     const {companyLogo, content, firstName,lastName,position,avatar} = props;
   return (
     <div className="row" style={{backgroundColor:'rgb(242, 244, 247)', borderRadius:'15px'}}>
-        <div className="col-7">
-            <div className="row mt-3">
+        <div className="col-6">
+            <div className="row mt-5">
                 <img  style={{width:'250px'}}src={companyLogo} alt="" />
             </div>
             <div className="row mt-4">
@@ -19,8 +19,8 @@ function CommentCard(props: ICommentCard) {
                 <a style={{textDecoration:'none'}} href="">Kullanıcı Hikayelerini Oku</a>
             </div>
         </div> 
-        <div className="col-5" style={{ backgroundColor:'rgb(242, 244, 247)', borderRadius:'15px'}}>
-            <img className='m-3' style={{borderRadius:'20px'}} src={avatar} alt="" />
+        <div className="col-6" style={{ backgroundColor:'rgb(242, 244, 247)', borderRadius:'15px'}}>
+            <img className='m-3 justify-content-center' style={{borderRadius:'20px', height:'500px', width:'500px'}} src={avatar} alt="" />
         </div>
     </div>
     
