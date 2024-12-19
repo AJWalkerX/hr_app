555716b0581b1e6e47d46bc352ec998de791ebfd
AhmetEris-dev <ahmet2_lock@hotmail.com>
2024-12-19 11:00:50 +0300

    userınformationPage oluşturuldu

--- src/pages/UserInformationPage.tsx
+++ src/pages/UserInformationPage.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import logo from '../img/ik-logo2.svg'
 
 function UserInformationPage() {
   return (
-   <div className="container">
+   <div className="container-fluid">
     <div className="row">
-        marabalar
+       <div className="col-2"></div>
+       <div className="col-8">
+        <div className="row">
+            <a className='navbar-brand d-flex align-items-center ms-5'>
+            <div className="col-6">
+            <img src={logo} style={{width:'50px',height:'50px'}} /> 
+            <span>KolaysaİK</span>
+         </div>
+         <div className="col-6"></div>
+            </a>
+         
+        </div>
+        <div className="row"></div>
+       </div>
+       <div className="col-2"></div>
     </div>
    </div>
   )
