7a4a5f33f0c333f32a53194edd7d2fa0bf79905c
Alexander Joseph Walker <a.j.walker99@outlook.com>
2024-12-24 10:57:10 +0000

    admin login problem fixed

--- src/RouterPage.tsx
+++ src/RouterPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
@@ -10,7 +10,11 @@ import AdminWaitCustomersPage from "./pages/AdminWaitCustomersPage";
 import SetNewPasswordPage from "./pages/SetNewPasswordPage";
 import { hrDispatch, hrUseSelector } from "./stores";
 import { useDispatch } from "react-redux";
-import { adminLogin, fetchAdminLogin } from "./stores/features/adminAuthSlice";
+import {
+  adminLogin,
+  adminLogout,
+  fetchAdminLogin,
+} from "./stores/features/adminAuthSlice";
 import UserInformationPage from "./pages/UserInformationPage";
 import UserProfileSettingsPage from "./pages/UserProfileSettingsPage";
 import ManagerHomePage from "./pages/ManagerHomePage";
@@ -18,35 +22,58 @@ import UserPermitPage from "./pages/UserPermitPage";
 
 function RouterPage() {
   const dispatch = useDispatch<hrDispatch>();
+  const location = useLocation();
   const isAdminLogin = hrUseSelector((state) => state.adminAuth.isAdminAuth);
   useEffect(() => {
     const adminToken = localStorage.getItem("adminToken");
     if (adminToken) {
       dispatch(adminLogin());
+    } else {
+      dispatch(adminLogout());
     }
-  }, []);
+  }, [dispatch]);
+  useEffect(() => {
+    if (!location.pathname.includes("/admin")) {
+      localStorage.removeItem("adminToken");
+      dispatch(adminLogout());
+    }
+  }, [dispatch, location.pathname]);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />}/>
-        <Route path="/login" element={<LoginPage />}/>
-        <Route path="/register" element={<RegisterPage />}/>
-        <Route path="/admin" element={isAdminLogin ? <AdminHomePage /> : <AdminLoginPage/>}/>
-        <Route path="/admin-login" element={<AdminLoginPage />}/>
-        <Route path="/admin/customers" element={isAdminLogin ? <AdminCustomersPage/> : <AdminLoginPage/>}/>
-        <Route path="/admin/wait-customers" element={isAdminLogin ? <AdminWaitCustomersPage/> : <AdminLoginPage/>}/>
-        <Route path="/user-information" element={<UserInformationPage />}/>
-        <Route path="/profile" element={<UserProfileSettingsPage />}/>
-        <Route path="/manager" element={<ManagerHomePage />}/>
-        <Route path="/manager-permit" element={<UserPermitPage />}/>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route
+        path="/admin"
+        element={isAdminLogin ? <AdminHomePage /> : <AdminLoginPage />}
+      />
+      <Route path="/admin-login" element={<AdminLoginPage />} />
+      <Route
+        path="/admin/customers"
+        element={isAdminLogin ? <AdminCustomersPage /> : <AdminLoginPage />}
+      />
+      <Route
+        path="/admin/wait-customers"
+        element={isAdminLogin ? <AdminWaitCustomersPage /> : <AdminLoginPage />}
+      />
+      <Route path="/user-information" element={<UserInformationPage />} />
+      <Route path="/profile" element={<UserProfileSettingsPage />} />
+      <Route path="/manager" element={<ManagerHomePage />} />
+      <Route path="/manager-permit" element={<UserPermitPage />} />
 
-        <Route
-          path="/set-new-password"
-          element={<SetNewPasswordPage />}
-        ></Route>
-      </Routes>
-    </BrowserRouter>
+      <Route path="/set-new-password" element={<SetNewPasswordPage />}></Route>
+    </Routes>
   );
 }
 
-export default RouterPage;
+export default function App() {
+  return (
+    <BrowserRouter
+      future={{
+        v7_relativeSplatPath: true,
+      }}
+    >
+      <RouterPage />
+    </BrowserRouter>
+  );
+}
--- src/stores/features/adminAuthSlice.tsx
+++ src/stores/features/adminAuthSlice.tsx
@@ -6,58 +6,68 @@ import Swal from "sweetalert2";
 import { authSlice } from ".";
 
 const initialAdminAuthState = {
-    isAdminAuth: false,
-    isAdminLoginLoading: false,
-  
-    admin: {},
+  isAdminAuth: false,
+  isAdminLoginLoading: false,
+
+  admin: {},
 };
 
 export const fetchAdminLogin = createAsyncThunk(
-    "adminAuth/fetchAdminLogin",
-    async (payload: IAdminLoginRequest) => {
-        const response = await fetch(apis.adminAuthService + "/adminlogin",{
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(payload),
-        }).then((data) => data.json());
-        return response;
+  "adminAuth/fetchAdminLogin",
+  async (payload: IAdminLoginRequest, { rejectWithValue }) => {
+    try {
+      const response = await fetch(apis.adminAuthService + "/adminlogin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
+      const data = await response.json();
+      if (!response.ok) {
+        return rejectWithValue(data); // Hatalı yanıt durumunda
+      }
+      return data; // Başarılı yanıt
+    } catch (error: any) {
+      return rejectWithValue({ message: "Ağ hatası", error });
     }
+  }
 );
 
 const adminAuthSlice = createSlice({
-    name: "adminAuth",
-    initialState: initialAdminAuthState,
-    reducers: {
-        adminLogout(state){
-            state.isAdminAuth = false
-        },
-        adminLogin(state){
-            state.isAdminAuth = true;
-        }
+  name: "adminAuth",
+  initialState: initialAdminAuthState,
+  reducers: {
+    adminLogout(state) {
+      state.isAdminAuth = false;
+      localStorage.removeItem("adminToken");
     },
-    extraReducers: (build) => {
-        build.addCase(fetchAdminLogin.pending, (state) => {
-            state.isAdminLoginLoading = true;
-        });
-        build.addCase(fetchAdminLogin.fulfilled,
-            (state, action: PayloadAction<IBaseResponse>) => {
-                state.isAdminLoginLoading = false;
-                if ( action.payload.code === 200) {
-                    state.isAdminAuth = true;
-                    localStorage.setItem("adminToken", action.payload.data);
-                }else{
-                    Swal.fire({
-                        icon: "error",
-                        title: "Hata!",
-                        text: action.payload.message
-                    });
-                }
-            }
-        )
-    }
+    adminLogin(state) {
+      state.isAdminAuth = true;
+    },
+  },
+  extraReducers: (build) => {
+    build.addCase(fetchAdminLogin.pending, (state) => {
+      state.isAdminLoginLoading = true;
+    });
+    build.addCase(
+      fetchAdminLogin.fulfilled,
+      (state, action: PayloadAction<IBaseResponse>) => {
+        state.isAdminLoginLoading = false;
+        if (action.payload.code === 200) {
+          state.isAdminAuth = true;
+          localStorage.setItem("adminToken", action.payload.data);
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Hata!",
+            text: action.payload.message,
+          });
+        }
+      }
+    );
+  },
 });
 
-export const {adminLogin,adminLogout} = adminAuthSlice.actions;
+export const { adminLogin, adminLogout } = adminAuthSlice.actions;
 export default adminAuthSlice.reducer;
