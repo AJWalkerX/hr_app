84c655b010151f50d390b7082e953d838ff9929c
EmineKarabolat <emine.karabolat.01@gmail.com>
2024-12-25 15:02:41 +0300

    permit sayfaları oluşturuldu

---
+++ src/pages/PermitRequestPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PermitRequestOrganism from '../components/organisms/PermistRequest/PermitRequestOrganism'
+
+function PermitRequestPage() {
+  return (
+    <div className="container">
+        
+        <div className="row">
+            <PermitRequestOrganism/>
+        </div>
+    </div>
+  )
+}
+
+export default PermitRequestPage
