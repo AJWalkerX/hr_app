cfb91ef01096d443637c262c26104f97f0e677b5
Alexander Joseph Walker <a.j.walker99@outlook.com>
2025-01-06 15:57:05 +0000

    shift list fetch added

--- src/pages/ShiftManagerPage.tsx
+++ src/pages/ShiftManagerPage.tsx
@@ -1,28 +1,29 @@
-import React from 'react'
-import ShiftRequestOrganism from '../components/organisms/ShiftRequest/ShiftRequestOrganism'
-import ManagerSidebar from '../components/molecules/ManagerPanel/ManagerSidebar'
-import CompanyShiftList from '../components/organisms/CompanyShiftList/CompanyShiftList'
-
+import React from "react";
+import ShiftRequestOrganism from "../components/organisms/ShiftRequest/ShiftRequestOrganism";
+import ManagerSidebar from "../components/molecules/ManagerPanel/ManagerSidebar";
+import CompanyShiftList from "../components/organisms/CompanyShiftList/CompanyShiftList";
 
 function ShiftManagerPage() {
   return (
     <div className="container">
-        
-    <div className="row">
-      <div className="col-1">
-        <ManagerSidebar/>
-      </div>
-      <div className="col-11 " style={{marginTop:'70px', marginLeft: '250px'}}>
       <div className="row">
-      <ShiftRequestOrganism/>
+        <div className="col-1">
+          <ManagerSidebar />
         </div>
-      <div className="row">
-            <CompanyShiftList/>
+        <div
+          className="col-11 "
+          style={{ marginTop: "70px", marginLeft: "250px" }}
+        >
+          <div className="row">
+            <ShiftRequestOrganism />
+          </div>
+          <div className="row mt-3 mb-5">
+            <CompanyShiftList />
+          </div>
         </div>
       </div>
     </div>
-</div>
-  )
+  );
 }
 
-export default ShiftManagerPage
+export default ShiftManagerPage;
