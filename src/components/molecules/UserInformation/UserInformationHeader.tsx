a77df3cbc9c102605b9229e296a9ba631744d3cc
Mehtor99 <mehtor99@gmail.com>
2025-01-08 16:03:25 +0300

    User Information sayfası modernize edildi.

--- src/components/molecules/UserInformation/UserInformationHeader.tsx
+++ src/components/molecules/UserInformation/UserInformationHeader.tsx
@@ -6,7 +6,7 @@ import './UserInformationHeader.css'
 function UserInformationHeader() {
   return (
     <div className="row text-center  ">
-        <p style={{fontSize: '40px', fontWeight:'bolder', color:'#006ea6'}} className='informationHeader-text ms-5 mt-3'>
+        <p style={{fontSize: '40px', fontWeight:'bolder', color:'rgb(10, 57, 129)'}} className='informationHeader-text ms-5 mt-3'>
           Bilgilerinizi Tamamlayın
         </p>
       </div>
--- src/pages/UserInformationPage.tsx
+++ src/pages/UserInformationPage.tsx
@@ -8,8 +8,8 @@ function UserInformationPage() {
   return (
     <div className="full-background">
       
-      <div className="container-information shadow-lg p-3 mb-5 rounded mt-3">
-        <div className="row mb-3 ">
+      <div className="container-information p-3  ">
+        <div className="row  ">
           <UserInformationHeader />
         </div>
 
